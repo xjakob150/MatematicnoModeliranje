138b50ff7c2a50d49abd58e39c257c3198d72589
xjakob150 <xjakob150@gmail.com>
2026-05-07 17:26:34 +0200

    spremembe

--- app.py
+++ app.py
@@ -189,4 +189,5 @@ def generate_post():
         return jsonify({"error": f"Strežniška napaka: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
